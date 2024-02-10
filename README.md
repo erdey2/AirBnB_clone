c83d8812950c25d082b1495ef02220bc274364a0
Erdey Syoum <erdeysyoum@gmail.com>
2024-02-10 02:48:13 -0800

    Base model

---
+++ README.md
@@ -0,0 +1,4 @@
+# Airbnb (ABNB) is an online marketplace that connects people
+# who want to rent out their homes with people looking for 
+# accommodations in specific locales
+ 
---
+++ models/base_model.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""the BaseModel module."""
+from uuid import uuid4
+from datetime import datetime
+
+
+class BaseModel:
+    """the BaseModel class of the AirBnB project."""
+    def __init__(self):
+        """Initialize the object."""
+        self.id = str(uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
+    def __str__(self):
+        """print the string representation of the object."""
+        return "{}".format(self.__dict__)
+
+    def save(self):
+        """update the date and time."""
+        self.updated_at = datetime.now()
+    def to_dict(self):
+        """Returns the dictionary representation of the object."""
+        rdict = self.__dict__.copy()
+        rdict['created_at'] = self.created_at.isoformat()
+        rdict['updated_at'] = self.updated_at.isoformat()
+        rdict['__class__'] = self.__class__.__name__
+        return rdict
