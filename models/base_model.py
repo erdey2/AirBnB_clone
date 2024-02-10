30fe71c3e1f61d4c6268eb5d14a25418218a886d
Erdey Syoum <erdeysyoum@gmail.com>
2024-02-10 03:05:18 -0800

    Base Model

--- models/base_model.py
+++ models/base_model.py
@@ -11,7 +11,7 @@ class BaseModel:
         self.id = str(uuid4())
         self.created_at = datetime.now()
         self.updated_at = datetime.now()
-    
+
     def __str__(self):
         """print the string representation of the object."""
         return "{}".format(self.__dict__)
@@ -19,7 +19,7 @@ class BaseModel:
     def save(self):
         """update the date and time."""
         self.updated_at = datetime.now()
-    
+
     def to_dict(self):
         """Returns the dictionary representation of the object."""
         rdict = self.__dict__.copy()
