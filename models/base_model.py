3c4813027ae9915a9b777dd77100a9eacb1163c1
Erdey Syoum <erdeysyoum@gmail.com>
2024-02-10 03:29:43 -0800

    Base Model

--- models/base_model.py
+++ models/base_model.py
@@ -6,11 +6,18 @@ from datetime import datetime
 
 class BaseModel:
     """the BaseModel class of the AirBnB project."""
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         """Initialize the object."""
+        tformat = "%Y-%m-%dT%H:%M:%S.%f"
         self.id = str(uuid4())
         self.created_at = datetime.now()
         self.updated_at = datetime.now()
+        if len(kwargs) != 0:
+            for key, value in kwargs.items():
+                if key == "created_at" or key == "updated_at":
+                    self.__dict__[key] = datetime.strptime(value, tformat)
+                else:
+                    self.__dict__[key] = value
 
     def __str__(self):
         """print the string representation of the object."""
