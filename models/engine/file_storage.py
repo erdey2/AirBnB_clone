7fcb81f16bb7499625ab5195455752e9870999f0
Erdey Syoum <erdeysyoum@gmail.com>
2024-02-11 03:27:29 -0800

    file storage created

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -5,31 +5,33 @@ import json
 
 class FileStorage:
     """File storage class."""
+    __file_path = "file.json"
+    __objects = {}
+
     def all(self):
         """Return the dict __objects."""
         return FileStorage.__objects
-    
+
     def new(self, obj):
         """sets in __objects."""
         classname = obj.__class__.__name__
         FileStorage.__objects["{}.{}".format(classname, obj.id)] = obj
 
-    def safe(self):
+    def save(self):
         """serialize __objects."""
         odict = FileStorage.__objects
         objdict = {obj:odict[obj].to_dict() for obj in odict.keys()}
-        with open(FileStorage.__file_path, "w") as f:
+        with open(FileStorage.__file_path, 'w') as f:
             json.dump(objdict, f)
 
     def reload(self):
         """deserialize the json file."""
         try:
             with open(FileStorage.__file_path) as f:
-                objdict = json.load(f):
-                    for obj in objdict.values():
-                        cls_name = obj["__class__"]
-                        del obj["__class__"]
-                        self.new(eval(cls_name)(**obj))
+                objdict = json.load(f)
+                for obj in objdict.values():
+                    cls_name = obj["__class__"]
+                    del obj["__class__"]
+                    self.new(eval(cls_name)(**obj))
         except FileNotFoundError:
             return
-
